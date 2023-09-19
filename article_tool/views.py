b046e0963918b426e696dbceb92a2b40afd9db1f
Eduardo Louhau <louhaudev@gmail.com>
2023-09-19 10:47:42 -0400

    Implement GET and POST functionality.

--- article_tool/views.py
+++ article_tool/views.py
@@ -1,9 +1,21 @@
 from django.http import JsonResponse
 from articles_api.models import Article
 from .serializers import ArticleSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
+
+@api_view(['GET', 'POST'])
 def article_list(request):
-    articles = Article.objects.all()
-    serializer = ArticleSerializer(articles, many=True)
-    return JsonResponse({'articles': serializer.data}, safe=False)
 
+    if request.method == 'GET':
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return JsonResponse({'articles': serializer.data})
+    
+    if request.method == 'POST':
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED) 
