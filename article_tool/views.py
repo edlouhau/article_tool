b45f47c3854b81cc6845ef2691e126ffb6aa4c3e
Eduardo Louhau <louhaudev@gmail.com>
2023-09-19 09:27:25 -0400

    Create views file.

---
+++ article_tool/views.py
@@ -0,0 +1,9 @@
+from django.http import JsonResponse
+from articles_api.models import Article
+from .serializers import ArticleSerializer
+
+def article_list(request):
+    articles = Article.objects.all()
+    serializer = ArticleSerializer(articles, many=True)
+    return JsonResponse(serializer.data)
+
